bbad6a202b07a3ac2508a6608eeeceb174f060a4
Blabla <st.ten.veldhuis@gmail.com>
2016-12-14 18:14:49 +0100

    Here goes nothing.

--- src/magicode/pureentities/entity/WalkingEntity.php
+++ src/magicode/pureentities/entity/WalkingEntity.php
@@ -157,10 +157,9 @@ abstract class WalkingEntity extends BaseEntity{
         if(!$isJump){
             if($this->onGround){
                 $block = $this->level->getBlock($this->add($dx, 0.2, $dz));
-                var_dump($block);
                 var_dump($block->isSolid());
                 if($block->isSolid()) {
-                    $this->motionY = 1;
+                    $this->motionY += 1;
                 } else {
                     $this->motionY = 0;
                 }
