1ce56cd488d4f910ac10c9c98e6f9e7a4cfa240f
Blabla <st.ten.veldhuis@gmail.com>
2016-12-15 16:42:39 +0100

    Fix some typos

--- src/magicode/pureentities/entity/WalkingEntity.php
+++ src/magicode/pureentities/entity/WalkingEntity.php
@@ -89,7 +89,7 @@ abstract class WalkingEntity extends BaseEntity{
             return false;
         }
 
-        $block = $this->level->getBlock(new Vector3($this->getDirectionPlain()->getX(), $this->getY(), $this->getDirectionPlayer()->getZ()));
+        $block = $this->level->getBlock(new Vector3($this->getDirectionPlane()->getX(), $this->getY(), $this->getDirectionPlane()->getZ()));
         if($block instanceof Slab || $block instanceof Stair){
             $this->motionY = 0.5;
             return true;
